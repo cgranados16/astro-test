9afc54e019c712482c2cedb1cc863bf6cc25c981
cgranados16 <cgranados@ltvco.com>
2023-11-09 14:14:06 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import lit from "@astrojs/lit";
 // https://astro.build/config
 export default defineConfig({
   integrations: [lit()],
+  vite: {
+    ssr: {
+      noExternal: ["@cgranados/material-web-library"],
+    },
+  },
 });
